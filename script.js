3d122c57004b10b5b83dd28471a36136a42e974c
Ellin860 <136501280+Ellin860@users.noreply.github.com>
2023-07-28 16:50:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Домашняя работа
+// Задача №1
+let tempDegreeCelsy = 20;
+let tempDegreeFarengeyt = (9/5)* tempDegreeCelsy + 32;
+console.log(tempDegreeCelsy);
+console.log(tempDegreeFarengeyt);
+
+// Задача №2
+let name = 'ELINA';
+let admin;
+admin = name;
+console.log(admin);
